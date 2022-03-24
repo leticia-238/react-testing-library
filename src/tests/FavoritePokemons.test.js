540554969bf17ecfca8c6129d534847334e41fe0
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-23 22:04:39 -0300

    Testa o componente FavoritePokemons.js - Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Testa o componente FavoritePokemons.js.', () => {
+  beforeEach(() => { localStorage.clear(); });
+
+  it('Exibe na tela a mensagem "No favorite pokemon found", se a pessoa não'
+  + ' tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const textMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(textMessage).toBeInTheDocument();
+  });
+
+  it('Exibe todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const pokemonNames = screen.getAllByTestId('pokemon-name');
+    const linkToPokemonDetails = screen.getByRole('link', { name: /More details/i });
+
+    expect(linkToPokemonDetails).toBeInTheDocument();
+    userEvent.click(linkToPokemonDetails);
+
+    const checkFavoritePokemon = screen.getByLabelText(/Pokémon favoritado?/i);
+
+    expect(checkFavoritePokemon).toBeInTheDocument();
+    userEvent.click(checkFavoritePokemon);
+
+    history.push('/favorites');
+
+    expect(pokemonNames).toHaveLength(1);
+  });
+});
